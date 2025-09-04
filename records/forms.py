a80b047ff353775f9693a9c0412b3fb786eb6df4
Albert Hamim <your-email@example.com>
2025-09-04 23:14:56 +0600

    update

---
+++ records/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    department = forms.CharField(max_length=100, required=True)
+    badge_number = forms.CharField(max_length=50, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'department', 
+                 'badge_number', 'password1', 'password2')
--- records/views.py
+++ records/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SignUpForm
+from django.contrib.auth import login
+from django.contrib.auth import authenticate, logout as auth_logout
 
 # Create your views here.
 def Signup(request):
-    return HttpResponse('ok')
+    if request.method =='POST' :
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('User created successfully')
+        else:
+            form = SignUpForm()
+    return render(request, 'records/signup.html', {'form': form})
 def Login(request): 
     return HttpResponse('ok')   
 def Logout(request):    
